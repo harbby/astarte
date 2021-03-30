7e8a09a168cbf8d820cab6d5b4db0a6cb62ec42d
harbby <yezhixinghai@gmail.com>
2021-03-30 13:24:11 +0800

    add spark pagerank dome

---
+++ spark-pagerank/src/main/scala/compare/spark/pagerank/PageRank.scala
@@ -0,0 +1,36 @@
+package compare.spark.pagerank
+
+import org.apache.spark.HashPartitioner
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
+object PageRank {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .appName("SparkPageRank")
+      .master("local[2]")
+      .getOrCreate()
+
+    val iters = 3
+
+    val lines: RDD[(Integer, Array[Int])] = spark.sparkContext.makeRDD(Array("/data/data/ClueWeb09_WG_50m.graph-txt"), 1)
+      .flatMap(file => new FileIteratorReader(file))
+
+    val links = lines.partitionBy(new HashPartitioner(2)).cache()
+    var ranks = links.mapValues(v => 1.0)
+
+    for (i <- 1 to iters) {
+      val contribs: RDD[(Integer, Double)] = links.join(ranks).values.flatMap { case (urls, rank) =>
+        val size = urls.size
+        urls.map(url => (scala.Int.box(url), rank / size))
+      }
+      ranks = contribs.reduceByKey(_ + _).mapValues(0.15 + 0.85 * _)
+    }
+
+    val output = ranks.mapPartitions(_.take(10)).collect()
+    output.foreach(tup => println(s"${tup._1} has rank:  ${tup._2} ."))
+
+    spark.stop()
+  }
+}
